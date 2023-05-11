1cd96f53959fed4f2df475ad942765f4cc4b36cb
leonraz58 <leonraz58@gmail.com>
2023-05-11 14:15:57 +0300

    Basic JS 91-100

---
+++ BasicJavascript/95.js
@@ -0,0 +1,10 @@
+// Setup
+const myArray = [];
+let i = 5;
+let j = 0;
+while (i>=0){
+  myArray[j]=i;
+  i--;
+  j++
+}
+// Only change code below this line
---
+++ BasicJavascript/96.js
@@ -0,0 +1,6 @@
+// Setup
+const myArray = [];
+for (let i = 0; i < 5; i++){
+  myArray[i]=i+1;
+}
+// Only change code below this line
